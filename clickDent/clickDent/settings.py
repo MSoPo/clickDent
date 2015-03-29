0d7f8d83f245ea1ffe8df8d89f0e4ac159c945fa
MSoPo <Miguesopo@gmail.com>
2015-03-28 20:14:49 -0600

    Update settings.py
    datos bd

--- clickDent/clickDent/settings.py
+++ clickDent/clickDent/settings.py
@@ -66,10 +66,12 @@ WSGI_APPLICATION = 'clickDent.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+    'NAME': 'clickdent',                     
+    'USER': 'usrclickdent',
+    'PASSWORD': 'miguesopo13',
+    'HOST': 'localhost',                    
+    'PORT': '', 
 }
 
 # Internationalization
